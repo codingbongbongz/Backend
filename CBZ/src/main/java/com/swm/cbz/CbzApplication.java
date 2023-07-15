ecba8d95f82600084c5690656de921f435295ccd
ysbc1247 <ystc1247@khu.ac.kr>
2023-07-15 17:59:52 +0900

    initialize project

---
+++ CBZ/src/main/java/com/swm/cbz/CbzApplication.java
@@ -0,0 +1,13 @@
+package com.swm.cbz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CbzApplication.class, args);
+    }
+
+}
---
+++ CBZ/src/test/java/com/swm/cbz/CbzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swm.cbz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
