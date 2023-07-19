9f2ac90742803b403a36be4c73ed3c783f752207
ysbc1247 <ystc1247@khu.ac.kr>
2023-07-19 17:26:14 +0900

    [add] JpaRepository 사용

--- CBZ/src/main/java/com/swm/cbz/repository/UserRepository.java
+++ CBZ/src/main/java/com/swm/cbz/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.swm.cbz.repository;
 
-public class UserRepository {
+import com.swm.cbz.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface UserRepository extends JpaRepository<User, String> {
 }
