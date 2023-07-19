45f19889d763d2bb0db855c4e7c4a7ac69078e3c
ysbc1247 <ystc1247@khu.ac.kr>
2023-07-19 18:35:21 +0900

    [add] link upload 위한 dto 구현

---
+++ CBZ/src/main/java/com/swm/cbz/dto/LinkUploadDTO.java
@@ -0,0 +1,12 @@
+package com.swm.cbz.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LinkUploadDTO {
+    private String username;
+
+    private String link;
+}
