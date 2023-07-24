5cabc496e49be508121815598de9b9f88e3aa9ac
ystc1247 <ystc1247@khu.ac.kr>
2023-07-24 13:03:03 +0900

    [feat] jparepository 함수추가

--- CBZ/src/main/java/com/swm/cbz/repository/TranscriptRepository.java
+++ CBZ/src/main/java/com/swm/cbz/repository/TranscriptRepository.java
@@ -4,6 +4,9 @@ import com.swm.cbz.domain.Transcript;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Optional;
+
 @RepositoryRestResource
 public interface TranscriptRepository extends JpaRepository<Transcript, Long> {
+    Optional<Transcript> findByIdAndVideoId(Long transcriptId, Long videoId);
 }
