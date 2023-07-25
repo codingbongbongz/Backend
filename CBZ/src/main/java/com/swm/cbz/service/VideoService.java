925b01f44618613a80c26487be8424a5ba65472d
ystc1247 <ystc1247@khu.ac.kr>
2023-07-25 09:31:12 +0900

    [fix] exception handling 추가

--- CBZ/src/main/java/com/swm/cbz/service/VideoService.java
+++ CBZ/src/main/java/com/swm/cbz/service/VideoService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.Optional;
 
 @Service
@@ -19,10 +20,10 @@ public class VideoService {
 
     public ResponseEntity<Video> uploadVideo(String username, String link){
         Optional<User> userOptional = userService.searchUserById(username);
-        if(userOptional.isPresent()){
-            return transcriptService.fetchTranscripts(link, username);
+        if(!userOptional.isPresent()){
+            throw new EntityNotFoundException("유저를 찾을 수 없습니다.");
         }
-        return null;
+        return transcriptService.fetchTranscripts(link, username);
     }
 
 }
