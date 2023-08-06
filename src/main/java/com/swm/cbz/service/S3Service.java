71c798777ca7f65f7671370e0b14b2ae97beb613
ystc1247 <ystc1247@khu.ac.kr>
2023-08-06 16:44:49 +0900

    [add] implemented S3Service

--- src/main/java/com/swm/cbz/service/S3Service.java
+++ src/main/java/com/swm/cbz/service/S3Service.java
@@ -1,5 +1,8 @@
 package com.swm.cbz.service;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.polly.AmazonPolly;
 import com.amazonaws.services.polly.AmazonPollyClientBuilder;
@@ -19,6 +22,7 @@ import com.swm.cbz.config.AWSConfig;
 import com.swm.cbz.domain.Transcript;
 import com.swm.cbz.repository.TranscriptRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
@@ -31,55 +35,21 @@ import java.util.UUID;
 
 @Service
 public class PollyService {
-    private String bucketName;
-    private String defaultKey;
-    private TranscriptRepository transcriptRepository;
-    private final AmazonPolly pollyClient;
-    private AmazonS3 s3Client;
-
-
+    private final TranscriptRepository transcriptRepository;
     @Autowired
-    public PollyService(AWSConfig awsConfig, TranscriptRepository transcriptRepository) throws IOException {
-        this.pollyClient = AmazonPollyClientBuilder.standard()
-                .withRegion(Regions.AP_NORTHEAST_2)
+    public PollyService(
+                        @Value("${aws.accessKey}") String awsAccessKeyId,
+                        @Value("${aws.secretKey}") String secretKey,
+                        @Value("${aws.region}") String region,
+                        @Value("${aws.s3.bucketName}") String bucketName,  TranscriptRepository transcriptRepository) throws IOException {
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(awsAccessKeyId, secretKey);
+        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
                 .build();
-
-        this.s3Client = AmazonS3ClientBuilder.standard()
-                .withRegion(Regions.AP_NORTHEAST_2) // Specify your region here.
-                .build();
-
-        this.bucketName = awsConfig.getBucketName();
-        this.defaultKey = awsConfig.getDefaultKey();
         this.transcriptRepository = transcriptRepository;
     }
 
-    public String synthesizeAndStore(String sentence) throws IOException {
-        // Polly 생성
-        SynthesizeSpeechRequest synthesizeSpeechRequest = new SynthesizeSpeechRequest()
-                .withOutputFormat(OutputFormat.Mp3)
-                .withVoiceId("Joanna")
-                .withText(sentence);
-
-        SynthesizeSpeechResult synthesizeSpeechResult = pollyClient.synthesizeSpeech(synthesizeSpeechRequest);
-
-        String key = defaultKey + "-" + UUID.randomUUID().toString() + ".mp3"; // Generating unique key using transcriptId
-        File audioFile = File.createTempFile(key, ".mp3");
-        try (InputStream inStream = synthesizeSpeechResult.getAudioStream();
-             OutputStream outStream = new FileOutputStream(audioFile)) {
-
-            byte[] buffer = new byte[1024];
-            int length;
-            while ((length = inStream.read(buffer)) > 0) {
-                outStream.write(buffer, 0, length);
-            }
-        }
-
-        // S3 업로드
-        s3Client.putObject(new PutObjectRequest(bucketName, key, audioFile));
-        // S3 링크 가져오기
-
-        return s3Client.getUrl(bucketName, key).toExternalForm();
-    }
 
     public ApiResponse<Resource> getTranscriptAudio(Long videoId, Long transcriptId) {
         Transcript transcript = transcriptRepository.findByTranscriptIdAndVideoVideoId(transcriptId, videoId)
