eb7321dd6b0ddbc9eb2706c42af41790c4a3a93a
ysbc1247 <ystc1247@khu.ac.kr>
2023-09-14 07:33:56 +0900

    [refactor] refactored polly controller

--- src/main/java/com/swm/cbz/controller/PollyController.java
+++ src/main/java/com/swm/cbz/controller/PollyController.java
@@ -1,9 +1,9 @@
 package com.swm.cbz.controller;
 
-import com.swm.cbz.common.response.ApiResponse;
 import com.swm.cbz.repository.TranscriptRepository;
 import com.swm.cbz.service.S3Service;
-import org.springframework.core.io.Resource;
+import com.swm.cbz.service.TranscriptService;
+import com.swm.cbz.service.VideoService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,14 +15,16 @@ public class PollyController {
     private final TranscriptRepository transcriptRepository;
     private final S3Service s3Service;
 
-    public PollyController(TranscriptRepository transcriptRepository, S3Service s3Service) {
+    private final TranscriptService transcriptService;
+    public PollyController(TranscriptRepository transcriptRepository, S3Service s3Service, TranscriptService transcriptService) {
         this.transcriptRepository = transcriptRepository;
         this.s3Service = s3Service;
+        this.transcriptService = transcriptService;
     }
 
     @GetMapping("/videos/{videoId}/transcripts/{transcriptId}/audio")
-    public ResponseEntity<byte[]> getTranscriptAudio(@PathVariable("videoId") Long videoId, @PathVariable("transcriptId") Long transcriptId) {
+    public ResponseEntity<byte[]> getTranscriptAudio(@PathVariable("videoId") Long videoId, @PathVariable("transcriptId") Long transcriptId) throws Exception {
+        transcriptService.audioProcessByTranscript(transcriptRepository.findByTranscriptId(transcriptId));
         return s3Service.getTranscriptAudio(videoId, transcriptId);
     }
-
 }
